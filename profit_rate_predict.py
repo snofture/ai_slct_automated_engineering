d0941cec3cf2dbf070f95af5ad7e4c834cb80683
unknown <limen@360buyad.local>
2017-07-10 17:10:33 +0800

    Big Change of sku_price calculating logic,now use gdm_m04_ord_det_sum

--- profit_rate_predict.py
+++ profit_rate_predict.py
@@ -63,6 +63,22 @@ for col in a_web.columns.difference(['sku_id','web_id']):
 
 
 
+
+#2import sku_price table
+gdm_m04_gdm_m04 = params['worker']['dir']+'/input/'+params['EndDate']+'/'+params['item_third_cate_cd']+'/gdm_m04_ord_det_sum'
+gdm_m04_ord_det_sum = pd.read_table('gdm_m04_ord_det_sum', sep='\t',encoding='utf-8')
+gdm_m04_ord_det_sum['sku_id'] = gdm_m04_ord_det_sum['item_sku_id']
+gdm_m04_ord_det_sum = gdm_m04_ord_det_sum.groupby(['sku_id']).agg({'before_prefr_amount':'sum','sale_qtty':'sum'})
+gdm_m04_ord_det_sum = gdm_m04_ord_det_sum.reset_index()
+gdm_m04_ord_det_sum['mean_price'] = gdm_m04_ord_det_sum['before_prefr_amount']/gdm_m04_ord_det_sum['sale_qtty']
+gdm_m04_ord_det_sum.drop(['before_prefr_amount','sale_qtty'], axis = 1, inplace = True)
+
+
+
+
+
+
+'''
 #import sku_price table
 sku_price = params['worker']['dir']+'/input/'+params['EndDate']+'/'+params['item_third_cate_cd']+'/sku_price'
 sku_price = pd.read_table('sku_price', sep='\t',encoding='utf-8')
@@ -75,10 +91,10 @@ sku_price.drop(['item_first_cate_cd','item_second_cate_cd',
 sku_price = sku_price[~np.isnan(sku_price['mean_price'])] 
     
 sku_price['sku_id'] = sku_price['sku_id'].apply(lambda x:int(x))
-    
+'''    
     
 #merge jd_pop_attrs table with sku_price table based on sku_id 
-jd_pop = pd.merge(a_web, sku_price, how = 'inner', on = 'sku_id')
+jd_pop = pd.merge(a_web, gdm_m04_ord_det_sum, how = 'inner', on = 'sku_id')
 jd_pop = jd_pop.drop_duplicates()
 
 
@@ -154,78 +170,77 @@ net_profit_percent = pd.merge(jd,average_profit, how = 'inner', on = 'sku_id')
 net_profit_percent.drop(['sku_id','web_id'],axis = 1, inplace = True)
 
 
-if __name__ == '__main__':    
+   
 
-    #train_test_split
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(net_profit_percent.drop('profit_rate',
-                                                                                axis=1), 
-                                                        net_profit_percent['profit_rate'], 
-                                                        test_size=0.30,
-                                                        random_state = 101)
-    
-    
-    #optimize algotirhm and tune parameter with GridSearchCV
-    from sklearn.grid_search import GridSearchCV
-    from sklearn.ensemble import RandomForestRegressor
-    rfr = RandomForestRegressor(  n_estimators = 500, 
-                                  max_features = 'auto',
-                                  max_depth=8,
-                                  min_samples_leaf=4,
-                                  min_samples_split=8,
-                                  oob_score=True,
-                                  #random_state = 42,
-                                  n_jobs=-1,
-                                  criterion = 'mae',
-                                  bootstrap = True)
-    param_grid = {
-    'n_estimators':[100,200,300,500],       
-    'max_depth':[3,5,8,10],
-    'min_samples_leaf':[2,4,6],
-    'min_samples_split':[4,8,10]
-    }
+#train_test_split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(net_profit_percent.drop('profit_rate',axis=1),+ \
+                                                    net_profit_percent['profit_rate'], + \
+                                                    test_size=0.30, + \
+                                                    random_state = 101)
 
-    CV_rfr= GridSearchCV(estimator=rfr, param_grid=param_grid, cv=5)
-    CV_rfr.fit(X_train, y_train) 
-    print (CV_rfr.best_params_)
-    
-    
-    #implement RandomForestregressor to solve regression problem    
-    from sklearn.ensemble import RandomForestRegressor
-    rfr = RandomForestRegressor(  n_estimators = CV_rfr.best_params_['n_estimators'], 
-                                  max_features = 'auto',
-                                  max_depth=CV_rfr.best_params_['max_depth'],
-                                  min_samples_leaf=CV_rfr.best_params_['min_samples_leaf'],
-                                  min_samples_split=CV_rfr.best_params_['min_samples_split'],
-                                  oob_score=True,
-                                  #random_state = 42,
-                                  criterion = 'mae',
-                                  n_jobs=-1,
-                                  bootstrap = True)
-                                  #warm_start=False,
-                                  #max_leaf_nodes = 30)
-    rfr.fit(X_train, y_train)
-    predictions = rfr.predict(X_test)
-    
-       
-    #plt.scatter(y_test,predictions)
-    print('MAE:', metrics.mean_absolute_error(y_test, predictions))
-    print('MSE:', metrics.mean_squared_error(y_test, predictions))
-    print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
-    print('MAPE:', mean_absolute_percentage_error(y_test,predictions))
-    
-       
-    #subplots method of matplotlib 
-    fig, axes = plt.subplots(nrows = 2, ncols = 1)
-    axes[0].scatter(y_test, predictions)
-    plt.sca(axes[1]) #Use the pyplot interface to change just one subplot
-    plt.xticks(range(X_train.shape[1]),X_train.columns, color='r')
-    axes[1].bar(range(X_train.shape[1]),rfr.feature_importances_, color= 'b',align = 'center')
-    
-    
-    #implement the profit_prediction algorihtm on pop skus
-    pop.drop(['sku_id','web_id'], axis = 1, inplace = True)
-    pop_predictions = rfr.predict(pop)
+
+#optimize algotirhm and tune parameter with GridSearchCV
+from sklearn.grid_search import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+rfr = RandomForestRegressor(  n_estimators = 500, 
+                              max_features = 'auto',
+                              max_depth=8,
+                              min_samples_leaf=4,
+                              min_samples_split=8,
+                              oob_score=True,
+                              #random_state = 42,
+                              n_jobs=-1,
+                              criterion = 'mae',
+                              bootstrap = True)
+param_grid = {
+'n_estimators':[100,200,300,500],       
+'max_depth':[3,5,8,10],
+'min_samples_leaf':[2,4,6],
+'min_samples_split':[4,8,10]
+}
+
+CV_rfr= GridSearchCV(estimator=rfr, param_grid=param_grid, cv=5)
+CV_rfr.fit(X_train, y_train) 
+print (CV_rfr.best_params_)
+
+best_model = CV_rfr.best_estimator_
+#implement RandomForestregressor to solve regression problem    
+#from sklearn.ensemble import RandomForestRegressor
+#rfr = RandomForestRegressor(  n_estimators = CV_rfr.best_params_['n_estimators'], 
+#                              max_features = 'auto',
+#                              max_depth=CV_rfr.best_params_['max_depth'],
+#                              min_samples_leaf=CV_rfr.best_params_['min_samples_leaf'],
+#                              min_samples_split=CV_rfr.best_params_['min_samples_split'],
+#                              oob_score=True,
+#                              #random_state = 42,
+#                              criterion = 'mae',
+#                              n_jobs=-1,
+#                              bootstrap = True)
+#                              #warm_start=False,
+#                              #max_leaf_nodes = 30)
+#rfr.fit(X_train, y_train)
+predictions = best_model.predict(X_test)
+
+   
+#plt.scatter(y_test,predictions)
+print('MAE:', metrics.mean_absolute_error(y_test, predictions))
+print('MSE:', metrics.mean_squared_error(y_test, predictions))
+print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+print('MAPE:', mean_absolute_percentage_error(y_test,predictions))
+
+   
+#subplots method of matplotlib 
+fig, axes = plt.subplots(nrows = 2, ncols = 1)
+axes[0].scatter(y_test, predictions)
+plt.sca(axes[1]) #Use the pyplot interface to change just one subplot
+plt.xticks(range(X_train.shape[1]),X_train.columns, color='r')
+axes[1].bar(range(X_train.shape[1]),rfr.feature_importances_, color= 'b',align = 'center')
+
+
+#implement the profit_prediction algorihtm on pop skus
+pop.drop(['sku_id','web_id'], axis = 1, inplace = True)
+pop_predictions = rfr.predict(pop)
 
 
 
